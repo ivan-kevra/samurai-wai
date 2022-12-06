da1457160e1e1e96007cf402ad82aa96d6c83a80
Ivan Kevra <78702646+ivankevra@users.noreply.github.com>
2022-12-06 12:53:08 +0300

    16th lesson

--- src/Components/Profile/Profile.tsx
+++ src/Components/Profile/Profile.tsx
@@ -1,8 +1,9 @@
 import React from "react";
+import s from './Profile.module.css';
 
 export const Profile = () => {
     return (
-        <div className={'content'}>
+        <div className={s.content}>
             <div>
                 <img
                     src='https://cdn.cnn.com/cnnnext/dam/assets/181010131059-australia-best-beaches-cossies-beach-cocos3-full-169.jpg'/>
@@ -11,8 +12,11 @@ export const Profile = () => {
             <div>
                 my posts
                 <div>new post</div>
-                <div>post 1</div>
-                <div>post 2</div>
+                <div className={s.posts}>
+                    <div className={s.item}>post 1</div>
+                    <div className={s.item}>post 2</div>
+                </div>
+
             </div>
         </div>
     )
